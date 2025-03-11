a51b8020a6371fc9c8183d3093d7a02589477c9a
iamnachiketh <nachikethkc@gmail.com>
2025-03-11 21:08:56 +0530

    [Nachiketha]Added: Implemented leap year check

--- app.py
+++ app.py
@@ -38,20 +38,35 @@
 
 # Percentage of heads vs tails
 
-import random
-def probability_calculate(n, hcount, tcount):
-    for i in range(n):
-        num = random.random()
-        if num > 0.5 : tcount = tcount + 1
-        else: hcount = hcount + 1
-    return [hcount, tcount]
+# import random
+# def probability_calculate(n, hcount, tcount):
+#     for i in range(n):
+#         num = random.random()
+#         if num > 0.5 : tcount = tcount + 1
+#         else: hcount = hcount + 1
+#     return [hcount, tcount]
 
-n = int(input("Enter the Number of times to flip a coin: "))
-if(n < 0):
-    print("Enter the a positive number")
-else:
-    res = probability_calculate(n, 0, 0)
-    print(f"Percentage of heads: {(res[0]/n)*100}\n Percentage of tails: {(res[1]/n)*100}")
+# n = int(input("Enter the Number of times to flip a coin: "))
+# if(n < 0):
+#     print("Enter the a positive number")
+# else:
+#     res = probability_calculate(n, 0, 0)
+#     print(f"Percentage of heads: {(res[0]/n)*100}\n Percentage of tails: {(res[1]/n)*100}")
+
+
+# Check Leap year
+def check_leapyear(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(f"{year} is a leap year.")
+    else:
+        print(f"{year} is not a leap year.")
+
+year = int(input("Enter a year: "))
+if 1000 <= year <= 9999: 
+    check_leapyear(year)
+    exit(0)
+print("Please entre the 4 digit year")
+
 
 
 
