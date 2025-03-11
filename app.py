f977647e01f2432ab1c7764424fcdc71d64637e8
iamnachiketh <nachikethkc@gmail.com>
2025-03-11 20:19:32 +0530

    [Nachiketha]Added: Implemented dynamicaly printing the name

--- app.py
+++ app.py
@@ -14,12 +14,21 @@
 
 # print(res)
 
-def factorial(n): 
-    res = 1
-    for i in range(1,n+1):
-        res = res * i
-    print(res)
+# def factorial(n): 
+#     res = 1
+#     for i in range(1,n+1):
+#         res = res * i
+#     print(res)
 
-n = int(input("Enter the number: "))
+# n = int(input("Enter the number: "))
 
-print(factorial(n))
+# print(factorial(n))
+
+
+# Print name dynamicaly
+def print_message(name):
+    print(f"Hello {name}, How are you?")
+
+name = input("Enter the username: ")
+print_message(name)
+
